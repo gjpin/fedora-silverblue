82904cacc4ee18355e4b5cf9dd2c8a05ad15ae10
gjpin <3874515+gjpin@users.noreply.github.com>
2022-09-23 00:05:25 +0100

    add gnome builder flatpak

--- setup.sh
+++ setup.sh
@@ -114,6 +114,7 @@ sudo flatpak install -y flathub rest.insomnia.Insomnia
 sudo flatpak install -y flathub org.gimp.GIMP
 sudo flatpak install -y flathub org.blender.Blender
 sudo flatpak install -y flathub org.gnome.Extensions
+sudo flatpak install -y flathub org.gnome.Builder
 sudo flatpak install -y flathub com.usebottles.bottles && \
     sudo flatpak override com.usebottles.bottles --filesystem=xdg-data/applications
 sudo flatpak install -y flathub org.kde.PlatformTheme.QGnomePlatform
